b3fc0a1087df37e678c920443292da8725d97f9f
peter <peter@peters-Mac-Pro.local>
2016-08-17 07:39:59 -0700

    basic framework

---
+++ seventeen.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# seventeen.py
+
+##############################################################################
+# Imports
+
+
+# Body
+def computer_turn(m_remain, last_removed):
+    ''' Returns # removed by computer. '''
+
+
+    # return c_remove
+
+def player_turn(marbles_remain):
+    ''' Returns # removed by player.
+        Handles input errors.'''
+
+    # return p_remove
+
+def seventeen(playlist=[]):
+    '''Game Loop. Takes a play argument for v2'''
+
+    # Variables
+    marbles_remain = 17
+    last_removed = 0
+    player_last = False
+    msg = "Let's play the game of Seventeen!"
+
+    # main loop condition
+    print(msg)
+    while marbles_remain > 17:
+        pass
+
+
+
+    # Game over message when main loop exits
+
+##############################################################################
+def main():
+    seventeen()
+
+if __name__ == '__main__':
+    main()
